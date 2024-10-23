3f5c53893c9d1d19452cf7ac7bdd4f1d59f6d396
FelixStrong <144284685+FelixStrong@users.noreply.github.com>
2024-10-23 07:12:02 +0200

    254

---
+++ random/import_as.py
@@ -0,0 +1,12 @@
+# @Time: 2024/10/22 
+# @Author: Qi Wang
+# @File: import_as
+# @Project: Python-leran-in-total
+# @Quelle:
+
+
+import math
+from math import sqrt as square_root  # 使用别名避免与其他函数冲突
+
+# 使用别名调用 sqrt 函数
+print(square_root(16))  # 输出: 4.0
